8c0cce9e21771604c5e7ccac6e4ca7775d6d296c
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-08-26 15:14:45 -0400

    Setup browserify configuration

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); //Runs a local dev server
 var open = require('gulp-open'); //Opens a  URL in the browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify');  //Transforms React JSX to JS
+var source = require('vinyl-source-stream'); //User convential text streams with Gulp 
 
 var config = {
 	port: 8000,
 	devBaseURL: 'http:://localhost',
 	paths: {
 		html: './src/*.html',
-		dist: './dist'
+		js: './src/**/**.js',
+		dist: './dist',
+		mainJs: './src/main.js'
 	}
 }
 //Start a local dev server
@@ -30,11 +35,21 @@ gulp.task('html', function() {
 	gulp.src(config.paths.html).pipe(gulp.dest(config.paths.dist)).pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-	gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function(){
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle().on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open']);
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open']);
 
 
 
